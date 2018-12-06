8cb2285a142dc04e3751400e751907bfd5ca7847
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2018-12-06 11:04:39 -0700

    fix js filename and start js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getRandomNumber(min, max) {
   return math.random() * (max - min) + min;
 }
 
---Update min and max ranges--
+// Update min and max ranges
 
 var minRange = document.getElementById("min-range");
 var maxRange = document.getElementById("max-range");
@@ -12,6 +12,7 @@ var updateButton = document.querySelector(“range-update”);
 function minNumFunction() {
 var x = document.getElementById(“min-range”).value;
 document.getElementById(“min”).innerText = x;
+console.log();
 }
 
 updateButton.addEventListener(‘click’, minNumFunction);
